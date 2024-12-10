500e74eb775e7b8a9c3282ffd828763d8b240e35
hejiean <hejiean@foxmail.com>
2024-12-10 05:51:54 -0800

    modify libmalloc.c, add param_len check

--- sample/libmalloc.c
+++ sample/libmalloc.c
@@ -17,7 +17,7 @@
     {                                                                                                                                           \
         if (idx >= MAX_ARR_LEN)                                                                                                                 \
         {                                                                                                                                       \
-            printf("[%s : line %d]  ERROR: out_idx %ld is bigger than max idx %ld\n", EXTRACT_BASE_FILE(__FILE__), __LINE__, idx, MAX_ARR_LEN); \
+            printf("[%s : line %d]  ERROR: out_idx %ld is bigger than max idx %d\n", EXTRACT_BASE_FILE(__FILE__), __LINE__, idx, MAX_ARR_LEN); \
             return -12;                                                                                                                         \
         }                                                                                                                                       \
     } while (0)
@@ -31,15 +31,29 @@
             return -12;                                                                                                   \
         }                                                                                                                 \
     } while (0)
+
+#define LEN_CHECK(used_len, max_len)                                                                                      \
+    do                                                                                                                    \
+    {                                                                                                                     \
+        if (used_len > max_len)                                                                                           \
+        {                                                                                                                 \
+            printf("[%s : line %d]  ERROR: used_len %d of %s is bigger than max len %d !\n",                              \
+                EXTRACT_BASE_FILE(__FILE__), __LINE__, used_len, __FUNCTION__, max_len);                                  \
+            return -12;                                                                                                   \
+        }                                                                                                                 \
+    } while (0)    
 static uint64_t ADDR_ARR[MAX_ARR_LEN] = {0};
 
 EXPORT int my_malloc(const long *params, int params_len)
 {
+    const static int used_len = 2; // this function need 2 input
+    LEN_CHECK(used_len, params_len);
+
     long len = params[0];
     long out_idx = params[1];
     IDX_CHECK(out_idx);
     void *addr = malloc(len);
-    if (!addr)
+    if (!addr)// this function need 1 input
         return -22;
 
     ADDR_ARR[out_idx] = (uint64_t)addr;
@@ -48,11 +62,14 @@ EXPORT int my_malloc(const long *params, int params_len)
 
 EXPORT int my_free(const long *params, int params_len)
 {
+    const static int used_len = 1; // this function need 1 input
+    LEN_CHECK(used_len, params_len);
+
     long in_idx = params[0];
     IDX_CHECK(in_idx);
     if (ADDR_ARR[in_idx] == 0)
     {
-        printf("invalid input idx %d\n", in_idx);
+        printf("invalid input idx %ld\n", in_idx);
         return -12;
     }
 
@@ -63,6 +80,9 @@ EXPORT int my_free(const long *params, int params_len)
 
 EXPORT int my_read32(const long *params, int params_len)
 {
+    const static int used_len = 2; // this function need 2 input
+    LEN_CHECK(used_len, params_len);
+
     long in_idx = params[0];
     long offset = params[1];
     IDX_CHECK(in_idx);
@@ -73,6 +93,9 @@ EXPORT int my_read32(const long *params, int params_len)
 
 EXPORT int my_write32(const long *params, int params_len)
 {
+    const static int used_len = 3; // this function need 3 input
+    LEN_CHECK(used_len, params_len);
+
     long in_idx = params[0];
     long offset = params[1];
     long val = params[2];
